cb813a764c1d9f32ce1ec86429fa14387a8102b5
Никита Толкач <nikitatolkac@MacBook-Pro-Nikita.local>
2017-10-04 18:55:14 +0300

    10 октября

--- gulpfile.js
+++ gulpfile.js
@@ -71,7 +71,7 @@ function styles() {
             rootSize : 16
         }))      
         .pipe(autoprefixer({
-            browsers: ['last 3 version', '> 1%', 'ie 8', 'ie 9', 'Opera 12.1'],
+            browsers: ['last 5 version', '> 1%', 'ie 8', 'ie 9', 'Opera 12.1'],
             cascade: false
         }))
         .pipe(rename({suffix: '.min'}))
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,84 +1,103 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var mainCarousel = $('.main').slick({
         infinite: true,
-        dots: true
+        dots: true,
+        arrows: false
     });
-    // $(document).on('click', '.header-nav__item--mob-nav', function(e){
-    //     e.preventDefault();
-    //     $('.header-mob-menu').toggleClass('header-mob-menu--active animated bounceInDown');
-    //     $(this).toggleClass('header-nav__item--mob-nav--active');
-    // });
+    // карусели
+    var carouselScreen2 = $('.screen2-main--slider').slick({
+        infinite: true,
+        draggable: false
+    });
+
     var carouselScreen3 = $('.screen3-main--slider').slick({
-        infinite: true
+        infinite: true,
+        arrows: false
     });
-    $(document).on('click', '.screen3-main-item-desc-arrows__left', function(e){
+    $(document).on('click', '.screen3-main-item-desc-arrows__left', function (e) {
         e.preventDefault();
         carouselScreen3.slick('slickPrev');
     })
-    $(document).on('click', '.screen3-main-item-desc-arrows__right', function(e){
+    $(document).on('click', '.screen3-main-item-desc-arrows__right', function (e) {
         e.preventDefault();
         carouselScreen3.slick('slickNext');
     })
     var carouselScreen6 = $('.screen6-main--slider').slick({
-        infinite: true
+        infinite: true,
+        arrows: false
     });
-    $(document).on('click', '.screen6-main-item-desc-arrows__left', function(e){
+    $(document).on('click', '.screen6-main-item-desc-arrows__left', function (e) {
         e.preventDefault();
         carouselScreen6.slick('slickPrev');
     })
-    $(document).on('click', '.screen6-main-item-desc-arrows__right', function(e){
+    $(document).on('click', '.screen6-main-item-desc-arrows__right', function (e) {
         e.preventDefault();
         carouselScreen6.slick('slickNext');
     })
     var carouselScreen7 = $('.screen7-main--slider').slick({
-        infinite: true
+        infinite: true,
+        arrows: false
     });
-    $(document).on('click', '.screen7-main-item-desc-arrows__left', function(e){
+    $(document).on('click', '.screen7-main-item-desc-arrows__left', function (e) {
         e.preventDefault();
         carouselScreen7.slick('slickPrev');
     })
-    $(document).on('click', '.screen7-main-item-desc-arrows__right', function(e){
+    $(document).on('click', '.screen7-main-item-desc-arrows__right', function (e) {
         e.preventDefault();
         carouselScreen7.slick('slickNext');
     })
     var carouselScreen14 = $('.screen14-main--slider').slick({
         infinite: true,
-        slidesToShow:4,
-        slidesToScroll:2
+        slidesToShow: 4,
+        draggable: false,
+        slidesToScroll: 2
     });
-    $(document).on('click', '.screen14-main-arr__left', function(e){
+    $(document).on('click', '.screen14-main-arr__left', function (e) {
         e.preventDefault();
         carouselScreen14.slick('slickPrev');
     })
-    $(document).on('click', '.screen14-main-arr__right', function(e){
+    $(document).on('click', '.screen14-main-arr__right', function (e) {
         e.preventDefault();
         carouselScreen14.slick('slickNext');
     })
+    var carouselScreen15 = $('.screen15-main--slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        draggable: false,
+        slidesToScroll: 2
+    });
     // menu
-    $(document).on('click', '.nav-open', function(e){
+    $(document).on('click', '.nav-open', function (e) {
         $('.nav').addClass('nav--active')
     })
-    $(document).on('click', '.nav-close', function(e){
+    $(document).on('click', '.nav-close', function (e) {
         $('.nav').removeClass('nav--active')
     })
-    $(document).on('click', '.nav-main a', function(e){
+    $(document).on('click', '.nav-main a', function (e) {
         slideIndex = $(this).index();
-        mainCarousel.slick( 'slickGoTo',parseInt(slideIndex) );
+        mainCarousel.slick('slickGoTo', parseInt(slideIndex));
         $('.nav').removeClass('nav--active')
     });
     $(document).mouseup(function (e) {
         var container = $(".nav");
-        if (container.has(e.target).length === 0){
+        if (container.has(e.target).length === 0) {
             container.removeClass('nav--active');
         }
     });
+    // pagination
+    $(document).on('click', '.controls a', function (e) {
+        slideIndex = $(this).index();
+        mainCarousel.slick('slickGoTo', parseInt(slideIndex));
+        $(this).addClass('controls-item--active');
+        $(this).siblings().removeClass('controls-item--active');
+    });
     // tabs
     var tabs = $('#tabs');
-    $('.tabs-content > div', tabs).each(function(i) {
+    $('.tabs-content > div', tabs).each(function (i) {
         if (i !== 0)
             $(this).hide(0);
     });
-    tabs.on('click', '.tabs-nav a', function(e) {
+    tabs.on('click', '.tabs-nav a', function (e) {
         e.preventDefault();
         var tabId = $(this).attr('href');
         $('.tabs-nav a', tabs).removeClass('screen5-top-list__item--active');
@@ -88,11 +107,11 @@ $(document).ready(function() {
     });
 
     var tabs1 = $('#tabs1');
-    $('.tabs-content > div', tabs1).each(function(i) {
+    $('.tabs-content > div', tabs1).each(function (i) {
         if (i !== 0)
             $(this).hide(0);
     });
-    tabs1.on('click', '.tabs-nav a', function(e) {
+    tabs1.on('click', '.tabs-nav a', function (e) {
         e.preventDefault();
         var tabId = $(this).attr('href');
         $('.tabs-nav a', tabs1).removeClass('screen5-top-list__item--active');
@@ -102,11 +121,11 @@ $(document).ready(function() {
     });
 
     var tabs2 = $('#tabs2');
-    $('.tabs-content > div', tabs2).each(function(i) {
+    $('.tabs-content > div', tabs2).each(function (i) {
         if (i !== 0)
             $(this).hide(0);
     });
-    tabs2.on('click', '.tabs-nav a', function(e) {
+    tabs2.on('click', '.tabs-nav a', function (e) {
         e.preventDefault();
         var tabId = $(this).attr('href');
         $('.tabs-nav a', tabs2).removeClass('screen12-main-top-left-link--active');
@@ -114,4 +133,63 @@ $(document).ready(function() {
         $('.tabs-content > div', tabs2).hide(0);
         $(tabId).show();
     });
+    $('[data-fancybox]').fancybox({});
+    $(document).on('click', '.popup__close', function (e) {
+        e.preventDefault();
+        $.fancybox.close();
+    })
+
+    // документы
+     $(document).on('click', '.screen16-main-item-top', function(){
+        $(this).siblings('.screen16-main-item-desc').slideToggle(300);
+        $(this).toggleClass('screen16-main-item-top--active');
+        $(this).parent().siblings().find('.screen16-main-item-desc').slideUp(300);
+        $(this).parents('.screen16-main-block').siblings().find('.screen16-main-item-desc').slideUp(300);
+        $(this).parent().siblings().find('.screen16-main-item-top').removeClass('screen16-main-item-top--active');
+        $(this).parents('.screen16-main-block').siblings().find('.screen16-main-item-top').removeClass('screen16-main-item-top--active');
+     });
+
+     $(function() {  
+        // $(".screen9-main-list-item").niceScroll({
+        //     cursorcolor: "#4A90E2"
+        // });
+    });
+
+
+    // var price = $('#price').val(),
+    //     firstMoney = $('#vsnos').val(),
+    //     time = $('#time').val(),
+    //     pS = 0.1035/12,
+    //     sK = price - firstMoney,
+    //     kP = time * 12,
+    //     totalPriceAll = 0;
+
+    // totalPriceAll = sK * (pS +(pS / (Math.pow((1+pS),kP) - 1)));
+    // console.log(totalPriceAll);
+    // $('.popup3-top-item').
+
+    // var pS = 0.1035/12,
+    // sK = this.price - this.firstMoney,
+    // kP = this.time * 12,
+    // totalPriceAll = 0;
+
+    // totalPriceAll = sK * (pS +(pS / (Math.pow((1+pS),kP) - 1)));
+    // return (Math.round(totalPriceAll))+" ₽";
+    ymaps.ready(init);
+    function init(){ 
+        myMap = new ymaps.Map("screen5-infra-map", {
+            center: [60.1285,30.3360],
+            zoom: 11
+        }); 
+        myPlacemark = new ymaps.Placemark([60.1285,30.3360], {
+            hintContent: 'Москва!',
+            preset: 'islands#redGlyphIcon',
+            balloonContent: 'Столица России',
+            iconGlyph: 'run',
+            iconGlyphColor: 'red'
+        });
+        
+        myMap.geoObjects.add(myPlacemark);
+    }
 });
+
